f423614d64dc7fe5576ad8329518539b589ccbd2
nonwaves <84790075+nonwaves@users.noreply.github.com>
2022-01-10 19:15:13 -0500

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import { Link } from 'gatsby'
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About Me">
+            <p>I'm learning how to use Gatsby!</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
