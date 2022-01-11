bcb045c6c68b4f9b5344e862c9a1a2d318af0cb3
nonwaves <84790075+nonwaves@users.noreply.github.com>
2022-01-10 19:36:07 -0500

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "GGM personal site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
